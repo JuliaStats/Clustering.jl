3dd11ae314f1d99b359000d6d69a26eff541f232
John Myles White <jmw@johnmyleswhite.com>
2013-01-19 17:47:39 -0500

    Update to new Julia file ops

--- src/Clustering.jl
+++ src/Clustering.jl
@@ -7,7 +7,7 @@ module Clustering
 
     export k_means, dp_means
 
-    include(joinpath(julia_pkgdir(), "Clustering", "src", "types.jl"))
-    include(joinpath(julia_pkgdir(), "Clustering", "src", "k_means.jl"))
-    include(joinpath(julia_pkgdir(), "Clustering", "src", "dp_means.jl"))
+    include("types.jl")
+    include("k_means.jl")
+    include("dp_means.jl")
 end
