07e9b37d3cee2b9a53b3e794d84f4edc2f238c03
Dahua Lin <lindahua@gmail.com>
2013-03-09 21:49:57 -0600

    minor modification

--- src/Clustering.jl
+++ src/Clustering.jl
@@ -3,16 +3,15 @@ require("Options")
 module Clustering
     using Distributions
     using OptionsMod
-	using MLBase
 
     # import Base.show
 
-    export k_means, dp_means
+    export k_means
 	export kmeans, kmeans!, kmeans_opts, update!
 
     include("types.jl")
     include("k_means.jl")
-    include("dp_means.jl")
+    # include("dp_means.jl")
 	
 	include("seeding.jl")
 	include("kmeans.jl")
