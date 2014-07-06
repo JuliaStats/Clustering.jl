90ee504d164212731766300657f097ad85577c0e
Chris Yocum <cyocum@gmail.com>
2014-07-06 17:40:02 +0100

    Strengthen tests for k-medoids

--- test/kmedoids.jl
+++ test/kmedoids.jl
@@ -7,6 +7,13 @@ using Clustering
 x = rand(100, 500)
 dist = pairwise(Euclidean(), x)
 
-@test isa(kmedoids(dist, 3), Clustering.KmedoidsResult)
+result = kmedoids(dist, 30)
+@test isa(result, Clustering.KmedoidsResult)
+# There should be no duplicate medoids
+@test length(result.medoids) == length(unique(result.medoids))
+# Every medoid should belong to its own cluster
+for i = 1:30
+    @test result.assignments[result.medoids[i]] == i
+end
 
 @test_throws ErrorException kmedoids(dist, 500)
