7c5034bcbc4078816bbb3200886b389f97a154d2
Velimir V Vesselinov (monty) <velimir.vesselinov@gmail.com>
2018-07-09 17:51:45 -0600

    0 silhouette score set for clusers of size 1 (#120)

--- test/silhouette.jl
+++ test/silhouette.jl
@@ -17,3 +17,8 @@ a = [1, 2, 1, 2]
 c = [2, 2]
 
 @test all(isapprox.(silhouettes(a, c, D), [0.0, -0.5, -0.5, 0.0]))
+
+a = [1, 1, 1, 2]
+c = [3, 1]
+
+@test all(isapprox.(silhouettes(a, c, D), [0.5, 0.5, -1/3, 0.0]))
