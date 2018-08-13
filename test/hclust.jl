6dfc73d16409e2812d43aa1930aaac9d42c950e8
Alexey Stukalov <astukalov@gmail.com>
2018-08-14 01:32:52 +0200

    add cutree() tests

--- test/hclust.jl
+++ test/hclust.jl
@@ -7,11 +7,19 @@ using Test
 include("hclust-generated-examples.jl")
 
 # test to make sure many random examples match R's implementation
-@testset "example \"$example\"" for example in examples
+@testset "example #$i" for (i, example) in enumerate(examples)
     h = hclust(example["D"], example["method"])
     @test h.merge == example["merge"]
     @test h.height ≈ example["height"] atol=1e-5
     @test h.order == example["order"]
+
+    @testset "cutree()" begin
+        # FIXME compare with R cuttree() result
+        cutn2 = cutree(h, k=2)
+        @test cutn2 isa Vector{Int}
+        @test length(cutn2) == length(h.order)
+        @test all(cl -> 1 <= cl <= 2, cutn2)
+    end
 end
 
 end
