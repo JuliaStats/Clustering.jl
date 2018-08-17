95b1e46d84e8525a13327adc3d211e2f852d6f67
Alexey Stukalov <astukalov@gmail.com>
2018-08-17 15:24:18 +0200

    silhouette(): relax signatures
    - allow abstract matrices
    - make `counts` param optional
    - add tests

--- src/silhouette.jl
+++ src/silhouette.jl
@@ -4,12 +4,10 @@
 # this function returns r of size (k, n), such that
 # r[i, j] is the sum of distances of all points from cluster i to sample j
 #
-function sil_aggregate_dists(k::Int, a::AbstractVector{Int}, dists::DenseMatrix{T}) where T<:Real
+function sil_aggregate_dists(k::Int, a::AbstractVector{Int}, dists::AbstractMatrix{T}) where T<:Real
     n = length(a)
-    r = zeros(k, n)
-    for j = 1:n
-        (1 <= a[j] <= k) || error("a[j] should have 1 <= a[j] <= k.")
-    end
+    S = typeof((one(T)+one(T))/2)
+    r = zeros(S, k, n)
     @inbounds for j = 1:n
         for i = 1:j-1
             r[a[i],j] += dists[i,j]
@@ -22,13 +20,17 @@ function sil_aggregate_dists(k::Int, a::AbstractVector{Int}, dists::DenseMatrix{
 end
 
 
-function silhouettes(assignments::Vector{Int},
-                     counts::AbstractVector{Int},
-                     dists::DenseMatrix{T}) where T<:Real
+function silhouettes(assignments::AbstractVector{<:Integer},
+                     counts::AbstractVector{<:Integer},
+                     dists::AbstractMatrix{T}) where T<:Real
 
     n = length(assignments)
     k = length(counts)
-    size(dists) == (n, n) || throw(DimensionMismatch("Inconsistent array dimensions."))
+    for j = 1:n
+        (1 <= assignments[j] <= k) || throw(ArgumentError("Bad assignments[$j]=$(assignments[j]): should be in 1:$k range."))
+    end
+    sum(counts) == n || throw(ArgumentError("Mismatch between assignments ($n) and counts (sum(counts)=$(sum(counts)))."))
+    size(dists) == (n, n) || throw(DimensionMismatch("The size of a distance matrix ($(size(dists))) doesn't match the length of assignment vector ($n)."))
 
     # compute average distance from each cluster to each point --> r
     r = sil_aggregate_dists(k, assignments, dists)
@@ -50,14 +52,15 @@ function silhouettes(assignments::Vector{Int},
     # compute a and b
     # a: average distance w.r.t. the assigned cluster
     # b: the minimum average distance w.r.t. other cluster
-    a = Vector{Float64}(undef, n)
-    b = Vector{Float64}(undef, n)
+    S = eltype(r)
+    a = Vector{S}(undef, n)
+    b = Vector{S}(undef, n)
 
     for j = 1:n
         l = assignments[j]
         a[j] = r[l, j]
 
-        v = Inf
+        v = S(Inf)
         p = -1
         for i = 1:k
             @inbounds rij = r[i,j]
@@ -81,5 +84,13 @@ function silhouettes(assignments::Vector{Int},
     return sil
 end
 
-silhouettes(R::ClusteringResult, dists::DenseMatrix) =
+silhouettes(R::ClusteringResult, dists::AbstractMatrix) =
     silhouettes(assignments(R), counts(R), dists)
+
+function silhouettes(assignments::AbstractVector{<:Integer}, dists::AbstractMatrix)
+    counts = fill(0, maximum(assignments))
+    for a in assignments
+        counts[a] += 1
+    end
+    silhouettes(assignments, counts, dists)
+end
