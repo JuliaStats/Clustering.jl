97769872e70b3a82ab5055f14028fcc976bf995b
Art Wild <wildart@gmail.com>
2019-08-13 16:01:40 -0400

    fixed function parameters

--- src/mutualinfo.jl
+++ src/mutualinfo.jl
@@ -1,27 +1,29 @@
 # Mutual Information
 
-function _mutualinfo(A::AbstractMatrix{<:Integer})
+function _mutualinfo(A::AbstractMatrix{<:Integer}, normed::Bool)
     N = sum(A)
     (N == 0.0) && return 0.0
 
-    aᵢ = sum(A, dims=2)
-    bⱼ = sum(A, dims=1)
+    rows = sum(A, dims=2)
+    cols = sum(A, dims=1)
     entA = entropy(A)
-    entArows = entropy(aᵢ)
-    entAcols = entropy(bⱼ)
+    entArows = entropy(rows)
+    entAcols = entropy(cols)
 
     hck = (entA - entAcols)/N
     hc = entArows/N + log(N)
     hk = entAcols/N + log(N)
 
     mi = hc - hck
-    nmi = 2*mi/(hc+hk)
-
-    return mi, nmi
+    return if normed
+        2*mi/(hc+hk)
+    else
+        mi
+    end
 end
 
 """
-    mutualinfo(a, b) -> Float64
+    mutualinfo(a, b; normed=true) -> Float64
 
 Compute the *mutual information* between the two clusterings of the same
 data points.
@@ -29,12 +31,10 @@ data points.
 `a` and `b` can be either [`ClusteringResult`](@ref) instances or
 assignments vectors (`AbstractVector{<:Integer}`).
 
-Returns a tuple of indices:
-  - mutual information
-  - normalized mutual information (sum)
+If `normed` paremeter is `true`, the normalized mutual information (sum) is returned.
 
 # References
 > Vinh, Epps, and Bailey, (2009). “Information theoretic measures for clusterings comparison”.
 Proceedings of the 26th Annual International Conference on Machine Learning - ICML ‘09.
 """
-mutualinfo(a, b) = _mutualinfo(counts(a, b))
+mutualinfo(a, b; normed::Bool=true) = _mutualinfo(counts(a, b), normed)
