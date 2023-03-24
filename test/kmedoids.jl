bb15e090a02c27cb6c22900624b684194acba4e4
Alexey Stukalov <astukalov@gmail.com>
2023-03-24 12:47:47 -0700

    kmedoids(): cleanup tests

--- test/kmedoids.jl
+++ test/kmedoids.jl
@@ -10,10 +10,10 @@ include("test_helpers.jl")
     @test_throws ArgumentError kmedoids(randn(2, 3), 1)
     @test_throws ArgumentError kmedoids(randn(2, 3), 4)
     dist = inv.(max.(pairwise(Euclidean(), randn(2, 3), dims=2), 0.1))
-    @test kmedoids(dist, 2) isa KmedoidsResult
+    @test @inferred(kmedoids(dist, 2)) isa KmedoidsResult
     @test_throws ArgumentError kmedoids(dist, 2, display=:mylog)
     for disp in keys(Clustering.DisplayLevels)
-        @test kmedoids(dist, 2, display=disp) isa KmedoidsResult
+        @test @inferred(kmedoids(dist, 2, display=disp)) isa KmedoidsResult
     end
 end
 
@@ -28,7 +28,7 @@ dist = pairwise(SqEuclidean(), X, dims=2)
 @assert size(dist) == (n, n)
 
 Random.seed!(34568)  # reset seed again to known state
-R = kmedoids(dist, k)
+R = @inferred(kmedoids(dist, k))
 @test isa(R, KmedoidsResult)
 @test nclusters(R) == k
 @test length(R.medoids) == length(unique(R.medoids))
@@ -48,36 +48,45 @@ R = kmedoids(dist, k)
     end
 end
 
-# k=1 and k=n cases
-x = pairwise(SqEuclidean(), [1 2 3; .1 .2 .3; 4 5.6 7], dims=2)
-kmed1 = kmedoids(x, 1)
-@test nclusters(kmed1) == 1
-@test assignments(kmed1) == [1, 1, 1]
-@test kmed1.medoids == [2]
-kmed3 = kmedoids(x, 3)
-@test nclusters(kmed3) == 3
-@test sort(assignments(kmed3)) == [1, 2, 3]
-@test sort(kmed3.medoids) == [1, 2, 3]
+@testset "Toy example #1" begin
+    pts = [1 2 3; .1 .2 .3; 4 5.6 7]
+    # k=1 and k=n cases
+    dists = pairwise(SqEuclidean(), pts, dims=2)
 
+    @testset "k=1" begin
+        kmed1 = @inferred(kmedoids(dists, 1))
+        @test nclusters(kmed1) == 1
+        @test assignments(kmed1) == [1, 1, 1]
+        @test kmed1.medoids == [2]
+    end
 
-# this data set has three obvious groups:
-# group 1: [1, 3, 4], values: [1, 2, 3]
-# group 2: [2, 5, 7], values: [6, 7, 8]
-# group 3: [6, 8, 9], values: [21, 20, 22]
-#
+    @testset "k=3" begin
+        kmed3 = @inferred(kmedoids(dists, 3))
+        @test nclusters(kmed3) == 3
+        @test sort(assignments(kmed3)) == [1, 2, 3]
+        @test sort(kmed3.medoids) == [1, 2, 3]
+    end
+end
 
-X = reshape(map(Float64, [1, 6, 2, 3, 7, 21, 8, 20, 22]), 1, 9)
-dist = pairwise(SqEuclidean(), X, dims=2)
+@testset "Toy example #2" begin
+    pts = reshape(map(Float64, [1, 6, 2, 3, 7, 21, 8, 20, 22]), 1, 9)
+    # this data set has three obvious groups:
+    # group 1: [1, 3, 4], values: [1, 2, 3]
+    # group 2: [2, 5, 7], values: [6, 7, 8]
+    # group 3: [6, 8, 9], values: [21, 20, 22]
 
-R = kmedoids!(dist, [1, 2, 6])
-@test isa(R, KmedoidsResult)
-@test nclusters(R) == 3
-@test R.medoids == [3, 5, 6]
-@test R.assignments == [1, 2, 1, 1, 2, 3, 2, 3, 3]
-@test counts(R) == [3, 3, 3]
-@test wcounts(R) == counts(R)
-@test R.costs ≈ [1, 1, 0, 1, 0, 0, 1, 1, 1]
-@test R.totalcost ≈ 6.0
-@test R.converged
+    dists = pairwise(SqEuclidean(), pts, dims=2)
+
+    R = @inferred(kmedoids!(dists, [1, 2, 6]))
+    @test isa(R, KmedoidsResult)
+    @test nclusters(R) == 3
+    @test R.medoids == [3, 5, 6]
+    @test R.assignments == [1, 2, 1, 1, 2, 3, 2, 3, 3]
+    @test counts(R) == [3, 3, 3]
+    @test wcounts(R) == counts(R)
+    @test R.costs ≈ [1, 1, 0, 1, 0, 0, 1, 1, 1]
+    @test R.totalcost ≈ 6.0
+    @test R.converged
+end
 
 end
