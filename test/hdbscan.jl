09ed174053adf7a7f3a734ae2a44d606c5f213fd
MommaWatasu <ascendwatson@gmail.com>
2024-04-27 20:44:41 +0900

    [test]update test
    changes sugges alyst

--- test/hdbscan.jl
+++ test/hdbscan.jl
@@ -7,13 +7,13 @@ using Clustering
     lower_x = [i for i in π/2:π/50:3/2*π]
     upper_y = sin.(upper_x) .+ rand(50)./10
     lower_y = cos.(lower_x) .+ rand(51)./10
-    data = hcat([lower_x; upper_x], [lower_y; upper_y])
+    data = hcat([lower_x; upper_x], [lower_y; upper_y])'
     #test for main function
     @test_throws DomainError hdbscan(data, 5, 0)
-    @test_throws ArgumentError hdbscan(data, 5, 3; gen_mst=false)
     @test_nowarn result = hdbscan(data, 5, 3)
 
     # tests for result
-    result = hdbscan(data, 5, 3)
-    @test sum([length(c) for c in result.clusters]) == 101
+    result = @inferred(hdbscan(data, 5, 3))
+    @test isa(result, HdbscanResult)
+    @test sum(length, result.clusters) == 101
 end
