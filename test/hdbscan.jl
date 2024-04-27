661edbc5b26409b5d4a684a1714c04d90f6ab548
Alexey Stukalov <alyst@users.noreply.github.com>
2024-04-27 13:54:18 -0700

    hdbscan test: small tweaks

--- test/hdbscan.jl
+++ test/hdbscan.jl
@@ -7,13 +7,14 @@ using Clustering
     lower_x = [i for i in π/2:π/50:3/2*π]
     upper_y = sin.(upper_x) .+ rand(50)./10
     lower_y = cos.(lower_x) .+ rand(51)./10
-    data = hcat([lower_x; upper_x], [lower_y; upper_y])'
+    data = hcat([lower_x; upper_x],
+                [lower_y; upper_y])'
     #test for main function
     @test_throws DomainError hdbscan(data, 5, 0)
-    @test_nowarn result = hdbscan(data, 5, 3)
+    @test_nowarn @inferred(hdbscan(data, 5, 3))
 
     # tests for result
     result = @inferred(hdbscan(data, 5, 3))
     @test isa(result, HdbscanResult)
-    @test sum(length, result.clusters) == 101
+    @test sum(length, result.clusters) == size(data, 2)
 end
